5f3d4ddec105c57e3fd920b5be772d48bdbbea82
Sumith Damodaran <sumithpd@gmail.com>
2021-06-15 00:17:13 +0100

    Controlling the animation with GestureDetector

--- animation/habit_tracker_flutter/lib/ui/home/home_page.dart
+++ animation/habit_tracker_flutter/lib/ui/home/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:habit_tracker_flutter/ui/task/animated_task.dart';
 import 'package:habit_tracker_flutter/ui/task/task_completion_ring.dart';
 import 'package:habit_tracker_flutter/ui/theming/app_theme.dart';
 
@@ -10,9 +11,7 @@ class HomePage extends StatelessWidget {
       body: Center(
         child: SizedBox(
           width: 240,
-          child: TaskCompletionRing(
-            progress: 0.4,
-          ),
+          child: AnimatedTask(),
         ),
       ),
     );
