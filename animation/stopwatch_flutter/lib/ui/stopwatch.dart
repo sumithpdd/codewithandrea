1f20635ce13d6deab721977eed799c916af297ba
Sumith Damodaran <sumithpd@gmail.com>
2021-06-14 16:23:03 +0100

    Creating a separate StopwatchRenderer widget

--- animation/stopwatch_flutter/lib/ui/stopwatch.dart
+++ animation/stopwatch_flutter/lib/ui/stopwatch.dart
@@ -3,6 +3,7 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:flutter/scheduler.dart';
 import 'package:stopwatch_flutter/ui/elapsed_time_text_basic.dart';
+import 'package:stopwatch_flutter/ui/stopwatch_renderer.dart';
 
 import 'elapsed_time_text.dart';
 
@@ -42,8 +43,6 @@ class _StopwatchState extends State<Stopwatch>
     // return ElapsedTimeTextBasic(
     //   elapsed: _elapsed,
     // );
-    return ElapsedTimeText(
-      elapsed: _elapsed,
-    );
+    return StopwatchRenderer(elapsed: _elapsed);
   }
 }
