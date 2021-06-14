6a04e62966a8338f7fb38bd646748fd7bd1e62ac
Sumith Damodaran <sumithpd@gmail.com>
2021-06-14 19:22:08 +0100

    Implementing the animated clock-hand

--- animation/stopwatch_flutter/lib/ui/stopwatch_renderer.dart
+++ animation/stopwatch_flutter/lib/ui/stopwatch_renderer.dart
@@ -1,6 +1,7 @@
 import 'dart:math';
 
 import 'package:flutter/material.dart';
+import 'package:stopwatch_flutter/ui/clock_hand.dart';
 
 import 'elapsed_time_text.dart';
 
@@ -22,10 +23,25 @@ class StopwatchRenderer extends StatelessWidget {
             border: Border.all(width: 3, color: Colors.orange),
             borderRadius: BorderRadius.circular(radius),
           ),
-        )
-        // ElapsedTimeText(
-        //   elapsed: elapsed,
-        // ),
+        ),
+        Positioned(
+          left: radius,
+          top: radius,
+          child: ClockHand(
+            rotationZAngle: pi + (2 * pi / 60000) * elapsed.inMilliseconds,
+            handThickness: 2,
+            handLength: radius,
+            color: Colors.orange,
+          ),
+        ),
+        Positioned(
+          left: 0,
+          right: 0,
+          top: radius * 1.3,
+          child: ElapsedTimeText(
+            elapsed: elapsed,
+          ),
+        ),
       ],
     );
   }
