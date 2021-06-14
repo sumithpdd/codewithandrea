a4695d511feb67d4848b44e2c9aece015ad8f937
Sumith Damodaran <sumithpd@gmail.com>
2021-06-14 11:17:46 +0100

    Animated Opacity

--- animation/intro_flutter_playground/lib/pages/animated_opacity_page.dart
+++ animation/intro_flutter_playground/lib/pages/animated_opacity_page.dart
@@ -6,6 +6,13 @@ class AnimatedOpacityPage extends StatefulWidget {
 }
 
 class AnimatedOpacityPageState extends State<AnimatedOpacityPage> {
+  double _opacity = 1.0;
+  void _toggleOpacity() {
+    setState(() {
+      _opacity = _opacity == 0 ? 1.0 : 0.0;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,15 +24,14 @@ class AnimatedOpacityPageState extends State<AnimatedOpacityPage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             AnimatedOpacity(
-              opacity: 1.0,
+              opacity: _opacity,
               duration: Duration(seconds: 1),
               curve: Curves.fastOutSlowIn,
               child: FlutterLogo(size: 200),
             ),
             ElevatedButton(
               child: Text('Fade Logo'),
-              // TODO: Implement
-              onPressed: null,
+              onPressed: _toggleOpacity,
             ),
           ],
         ),
