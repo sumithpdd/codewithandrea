99f3d4c02a449f0d86076e8e14a0c116fcb5125a
Sumith Damodaran <sumithpd@gmail.com>
2021-06-14 15:07:14 +0100

    Tween Animation Builder

--- animation/intro_flutter_playground/README.md
+++ animation/intro_flutter_playground/README.md
@@ -14,3 +14,21 @@ A few resources to get you started if this is your first Flutter project:
 For help getting started with Flutter, view our
 [online documentation](https://flutter.dev/docs), which offers tutorials,
 samples, guidance on mobile development, and a full API reference.
+
+## Links and Resources about Implicit Animations
+
+Here are some links to official resources about Implicit Animations in Flutter:
+
+- Introduction to animations | Flutter.dev
+<https://flutter.dev/docs/development/ui/animations>
+
+- Implicit animations | Flutter.dev
+<https://flutter.dev/docs/development/ui/animations/implicit-animations>
+- Implicit animations codelab | Flutter.dev
+<https://flutter.dev/docs/codelabs/implicit-animations>
+- Flutter Animations Gallery: web demo
+<https://flutter.github.io/samples/web/animations/#/>,
+source code
+<https://github.com/flutter/samples/tree/master/animations>
+- HSL Colors Explained: What they are and when to use them in Dart/Flutter
+<https://codewithandrea.com/articles/hsl-colors-explained-flutter/>
--- animation/intro_flutter_playground/lib/pages/tween_animation_builder_page.dart
+++ animation/intro_flutter_playground/lib/pages/tween_animation_builder_page.dart
@@ -20,8 +20,53 @@ class HSVColorSelector extends StatefulWidget {
 }
 
 class _HSVColorSelectorState extends State<HSVColorSelector> {
+  double _hue = 0.0;
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        AnimatedContainer(
+          duration: Duration(milliseconds: 1500),
+          width: 200,
+          height: 200,
+          color: HSVColor.fromAHSV(1.0, _hue, 1.0, 1.0).toColor(),
+        ),
+        SizedBox(
+          height: 48.0,
+        ),
+        TweenAnimationBuilder<double>(
+          tween: Tween<double>(begin: 0.0, end: _hue),
+          duration: Duration(milliseconds: 1500),
+          builder: (context, hue, child) {
+            final hsvColor = HSVColor.fromAHSV(1.0, hue, 1.0, 1.0);
+            return Container(
+              width: 200,
+              height: 200,
+              color: hsvColor.toColor(),
+            );
+          },
+        ),
+        SizedBox(
+          height: 48.0,
+        ),
+        Container(
+          height: 30.0,
+          decoration: BoxDecoration(
+              gradient: LinearGradient(colors: [
+            for (var h = 0; h <= 360; h++)
+              HSVColor.fromAHSV(1.0, h.toDouble(), 1.0, 1.0).toColor()
+          ], stops: [
+            for (var h = 0; h <= 360; h++) h.toDouble() / 360,
+          ])),
+        ),
+        Slider.adaptive(
+          value: _hue,
+          min: 0,
+          max: 360,
+          onChanged: (value) => setState(() => _hue = value),
+        ),
+      ],
+    );
   }
 }
