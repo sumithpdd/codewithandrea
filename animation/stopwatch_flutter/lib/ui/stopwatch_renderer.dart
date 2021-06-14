08181ed2c0365c153e8448cae5a2cc632a015b41
Sumith Damodaran <sumithpd@gmail.com>
2021-06-14 19:46:08 +0100

    Completing the stopwatch UI

--- animation/stopwatch_flutter/lib/ui/stopwatch_renderer.dart
+++ animation/stopwatch_flutter/lib/ui/stopwatch_renderer.dart
@@ -2,6 +2,7 @@ import 'dart:math';
 
 import 'package:flutter/material.dart';
 import 'package:stopwatch_flutter/ui/clock_hand.dart';
+import 'package:stopwatch_flutter/ui/clock_markers.dart';
 
 import 'elapsed_time_text.dart';
 
@@ -18,12 +19,25 @@ class StopwatchRenderer extends StatelessWidget {
   Widget build(BuildContext context) {
     return Stack(
       children: [
-        Container(
-          decoration: BoxDecoration(
-            border: Border.all(width: 3, color: Colors.orange),
-            borderRadius: BorderRadius.circular(radius),
+        for (var i = 0; i < 60; i++)
+          Positioned(
+            left: radius,
+            top: radius,
+            child: ClockSecondsTickMarker(
+              seconds: i,
+              radius: radius,
+            ),
           ),
-        ),
+        for (var i = 5; i <= 60; i += 5)
+          Positioned(
+            left: radius,
+            top: radius,
+            child: ClockTextMarker(
+              value: i,
+              maxValue: 60,
+              radius: radius,
+            ),
+          ),
         Positioned(
           left: radius,
           top: radius,
